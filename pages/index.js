5261fc896a2f25df2251e418a91548ae8f927b22
Demolux <marvin.bauch@gmx.de>
2025-06-08 14:41:39 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', padding: '5rem' }}>
+      <h1>🚀 Willkommen bei Voludyne</h1>
+      <p>Dein 3D-Druck Pitchdeck powered by Next.js.</p>
+    </div>
+  );
+}
