c91d02ca3279665840465a97d904c8d8f23772e4
Demolux <marvin.bauch@gmx.de>
2025-06-11 19:16:14 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,6 @@ export default function Home() {
 		  scrollSnapType: 'y mandatory',
 		}}>
       <Logo />
-	  <ScrollProgress />
 	  <SlideIntro /> 
       <SlideProblem />
       <SlideSolution />
